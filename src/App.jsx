8c5b870364da13b4c4bd33d72fc227cfe79825cd
Akram-1022 <akram.ansari310174@gmail.com>
2024-09-11 19:45:26 +0530

    code finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  // const [count, setCount] = useState(0)
+  const [data , setData] = useState(null);
+
+  useEffect(()=>{
+    fetch('/api',{
+      method: "GET",
+      headers: {
+        'Content-Type': "application/json",
+      },
+    })
+    .then(response => response.json())
+    .then(result =>{
+      setData(result);
+      console.log(data);
+      
+    })
+    .catch(error=> console.log('Error fetching data:  ', error));
+  }, []);
 
   return (
     <>
-      <div>
+      {data? <pre>{JSON.stringify(data)}</pre> : 'Loading...'}
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -24,7 +44,7 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-      </div>
+      </div> */}
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
